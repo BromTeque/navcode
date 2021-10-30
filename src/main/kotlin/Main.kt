853a6f3183911202221d2604afd8a95416d3e401
Aleksander M Lingstad <aleksander@lingstad.no>
2021-10-30 10:52:45 +0200

    V1.1 - Tightened redundancy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,16 @@ import kotlin.math.ceil
 
 fun calculateUnemploymentBenefits(income: List<Double>) {
     // Variables
-    val grunnbelop: Int = 106399 //Grunnbeløp per year
-    var dagpengegrunnlag: Double = 0.0
-    var dagsats: Int = 0
+    val grunnbelop = 106399 //Grunnbeløp per year
 
     // Eligibility Calculation
     if ((income.sum() > 3 * grunnbelop) || (income[0] > 1.5 * grunnbelop)) {
-        dagpengegrunnlag = maxOf(income[0], (income.slice(0..2).sum())/3)                                     // Dagpengegrunnlag Formula
+        var dagpengegrunnlag = maxOf(income[0], (income.slice(0..2).sum())/3)                                     // Dagpengegrunnlag Formula
         if (dagpengegrunnlag > 6 * grunnbelop) {
             dagpengegrunnlag = 6 * grunnbelop.toDouble()
         }
-        dagsats = ceil(dagpengegrunnlag / 260).toInt()                                                               // Dagsats Formula
-        println("Eligible for dagpanger with a rate of $dagsats")
+        val dagsats = ceil(dagpengegrunnlag / 260).toInt()                                                               // Dagsats Formula
+        println("Eligible for dagpenger with a rate of $dagsats")
         return
     } else {
         println("Not eligible for dagpenger.")
@@ -22,8 +20,8 @@ fun calculateUnemploymentBenefits(income: List<Double>) {
 }
 
 fun main() {
-    val exampleResult = calculateUnemploymentBenefits(listOf(500000.0, 450000.0, 400000.0))
-    val above6GResult = calculateUnemploymentBenefits(listOf(800000.0, 750000.0, 700000.0))
-    val below3GResult = calculateUnemploymentBenefits(listOf(100000.0, 100000.0, 100000.0))
-    val below1p5GResult = calculateUnemploymentBenefits(listOf(111000.0, 0.0, 0.0))
+    calculateUnemploymentBenefits(listOf(500000.0, 450000.0, 400000.0))
+    calculateUnemploymentBenefits(listOf(800000.0, 750000.0, 700000.0))
+    calculateUnemploymentBenefits(listOf(100000.0, 100000.0, 100000.0))
+    calculateUnemploymentBenefits(listOf(111000.0, 0.0, 0.0))
 }
