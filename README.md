083efeb5c3891d321b0296cd151a37ed18f942d9
Aleksander M Lingstad <aleksander@lingstad.no>
2021-10-30 10:06:23 +0200

    Prep and planning. Syntax testing.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # navcode
+
 NAV IT - Kodeoppgave
+
+
+## Assumptions
+
+Year is 2021 and grunnbeløp is from 2021.5.1: 106 399 kr
+
+Income given is always from the last three years, given in the order of last year, year before, and the earliest year.
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,48 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.ceil
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun calculateUnemploymentBenefits(income: List<Double>): Double {
+    // Variables
+    val grunnbelop: Int = 106399 //Grunnbeløp per year
+    var dagpengegrunnlag: Double = 0.0
+    var dagsats: Int = 0
+
+
+    dagpengegrunnlag = grunnbelop.toDouble()
+    for (y in income) {
+        dagpengegrunnlag += income[income.indexOf(y)]
+    }
+    if (income.sum() > 3 * grunnbelop) {
+        println("dagpenger!")
+        println(income.sum())
+    } else if (income[0] > 1.5 * grunnbelop) {
+        println("dagpenger!")
+        println(income[0])
+    }
+    if (income.sum() > 6 * grunnbelop) {
+        println("for mye penger!")
+    }
+    print("Income last year: ")
+    println(income[0])
+    print("Average of income last 3 years: ")
+    println((income.slice(0..2).sum())/3)
+    if (dagpengegrunnlag > 6 * grunnbelop) {
+        println("benefits too big")
+        dagpengegrunnlag = 6 * grunnbelop.toDouble()
+    }
+
+    dagsats = ceil(dagpengegrunnlag / 260).toInt()
+    print("dagsats: ")
+    println(dagsats)
+
+
+    return dagpengegrunnlag
 }
+
+fun main() {
+    println("main")
+    val result = calculateUnemploymentBenefits(listOf(500000.0, 450000.0, 400000.0))
+    println(result)
+}
+
+//ASSUMPTIONS:
+// Income is ALWAYS given least year, year before, year before...
