b1a5d4024c1b9b09bf58d5a4b09bf9007355bc7e
Aleksander M Lingstad <aleksander@lingstad.no>
2021-10-31 14:38:17 +0100

    Updated readme, added additional test, and added variable arbeidsdager

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ The appropriate code is located at `./src/main/kotlin/Main.kt`
 
 ## Assumptions
 
-Year is 2021 and grunnbeløp is from 2021.5.1: 106 399 kr
+Year is 2021 and grunnbeløp (G) is 2021 average: 104 716 kr
 
 Income given is always from the last three years, given in the order of last year, year before, and the earliest year. E.g. 2020, 2019, 2018
+
+Code is API-like and user input is handled by a potential "frontend".
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,19 @@ import kotlin.math.ceil
 fun calculateUnemploymentBenefits(income: List<Double>) {
     // Variables
     val grunnbelop = 104716 // Gjennomsnitt per år (2021)
+    val arbeidsdager = 260 // NAV definerte arbeidsdager per år
 
     // Eligibility calculation
-    if ((income.sum() > 3 * grunnbelop) || (income[0] > 1.5 * grunnbelop)) {
+    if ((income[0] > 1.5 * grunnbelop) || (income.slice(0..2).sum() > 3 * grunnbelop)) {
         // Dagpenger formula and over 6G check
         var dagpengegrunnlag = maxOf(income[0], (income.slice(0..2).sum())/3)
         if (dagpengegrunnlag > 6 * grunnbelop) {
             dagpengegrunnlag = 6 * grunnbelop.toDouble()
         }
+
         // Dagsats formula
-        val dagsats = ceil(dagpengegrunnlag / 260).toInt()
+        val dagsats = ceil(dagpengegrunnlag / arbeidsdager).toInt()
+
         println("Kvalifisert for dagpenger med en dagsats på $dagsats kr")
         return
     } else {
@@ -24,8 +27,10 @@ fun calculateUnemploymentBenefits(income: List<Double>) {
 fun main() {
     print("Test: eksempel: ")
     calculateUnemploymentBenefits(listOf(500000.0, 450000.0, 400000.0))
-    print("Test: dagpengegrunnlag er over 6G: ")
+    print("Test: dagpengegrunnlag er over 6G over siste 3 år: ")
     calculateUnemploymentBenefits(listOf(800000.0, 770000.0, 730000.0))
+    print("Test: dagpengegrunnlag er over 6G over siste år: ")
+    calculateUnemploymentBenefits(listOf(1000000.0, 0.0, 0.0))
     print("Test: inntekt er under 3G: ")
     calculateUnemploymentBenefits(listOf(100000.0, 90000.0, 110000.0))
     print("Test: inntekt er under 1.5G: ")
